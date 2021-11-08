c386f5a0e52b3780e1e1710e46a848774e02a9bf
钱丽云 <376084782@qq.com>
2021-11-08 17:21:20 +0800

    no message

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -82,7 +82,7 @@ export default class socketManager {
             if (result.result == 1) {
             rsv(result.data);
           } else {
-            rej(result);
+            rsv(result);
           }
         }
       });
